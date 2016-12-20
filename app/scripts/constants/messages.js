480b3f04b17d3667ced68c5283abcaa748a0fec2
FauzanKhan <fauzankhan93@gmail.com>
2016-12-20 15:41:55 +0100

    Changes for failing code climate

--- app/scripts/constants/messages.js
+++ app/scripts/constants/messages.js
@@ -1,8 +1,8 @@
 app.constant('messages', {
-  noResults: "The User does not exist.",
-  noResponse: "Something went wrong. Please Try Again!",
-  noRepos: "The user has no Repositories",
-  inProgress: "Searching...",
-  fetchingData: "Fetching Data...",
-  failedRequest: "Failed Request. Please Try Again!"
+    noResults: "The User does not exist.",
+    noResponse: "Something went wrong. Please Try Again!",
+    noRepos: "The user has no Repositories",
+    inProgress: "Searching...",
+    fetchingData: "Fetching Data...",
+    failedRequest: "Failed Request. Please Try Again!"
 });
